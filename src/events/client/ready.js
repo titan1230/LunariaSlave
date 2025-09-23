1398312112f1230da9f3ad8935f3f96867382e62
Akshat Jain <akshat00jain@gmail.com>
2025-09-23 20:55:58 +0530

    feat: add userLuck table creation in database initialization

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -31,6 +31,7 @@ module.exports = {
 
         try {
             client.db.exec("CREATE TABLE IF NOT EXISTS serverChannels (id INTEGER PRIMARY KEY default 1,suggestionChannel varchar(255), loggingChannel varchar(255), suggestionApprovalChannel varchar(255))");
+            client.db.exec("CREATE TABLE IF NOT EXISTS userLuck (userID varchar(255) PRIMARY KEY, luck INT default 0)");
         } catch (err) {
             console.error("[DB ERROR]", err);
         }
