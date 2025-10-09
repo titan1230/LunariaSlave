129a867e1a2d90c28a3c83312ea3d4b11fc4f401
Akshat Jain <akshat00jain@gmail.com>
2025-10-09 19:24:27 +0530

    fix: enhance EVs formatting in getSmogonDescription function

--- src/utils/getSmogonDescription.js
+++ src/utils/getSmogonDescription.js
@@ -6,11 +6,21 @@ function getSmogonDescription(smogonData) {
     const abilityText = Array.isArray(ability) ? `**Ability:** ${ability.join(" / ")}\n` : `**Ability:** ${ability}\n`;
     const itemText = Array.isArray(item) ? `**Item:** ${item.join(" / ")}\n` : `**Item:** ${item}\n`;
     const movesText = moves && moves.length > 0 ? `**Moves:**\n- ${moves.map(move => Array.isArray(move) ? move.join(" / ") : move).join("\n- ")}\n` : "";
-    
-    const evsText = Object.entries(evs)
-        .map(([key, value]) => `${key}: ${value}`)
-        .join(" | ");
-    const evsLine = evsText ? `**EVs:** ${evsText}\n` : "";
+
+    let evsLine = "**EVs:**\n";
+    if (Array.isArray(evs)) {
+        evs.forEach((val) => {
+            const evsText = Object.entries(val)
+                .map(([key, value]) => `${key.toUpperCase()}: ${value.toString().padStart(3, '0')}`)
+                .join(" | ");
+            evsLine += evsText ? `- ${evsText}\n` : "";
+        });
+    } else {
+        const evsText = Object.entries(evs)
+            .map(([key, value]) => `${key.toUpperCase()}: ${value.toString().padStart(3, '0')}`)
+            .join(" | ");
+        evsLine += evsText ? `- ${evsText}\n` : "";
+    }
 
     const natureText = Array.isArray(nature) ? `**Nature:** ${nature.join(" / ")}\n` : `**Nature:** ${nature}\n`;
     const teraTypesText = teratypes ? Array.isArray(teratypes) ? `**Tera Type:** ${teratypes.join(" / ")}\n` : `**Tera Type:** ${teratypes}\n` : "";
