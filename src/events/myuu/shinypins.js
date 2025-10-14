419f990883394b2631c29805b8ef4880ae5eceb2
Akshat Jain <akshat00jain@gmail.com>
2025-10-14 18:58:07 +0530

    feat: implement message forwarding for specific user embeds

---
+++ src/events/myuu/shinypins.js
@@ -0,0 +1,30 @@
+const { EmbedBuilder } = require('discord.js');
+const { Events } = require('discord.js');
+
+module.exports = {
+    name: Events.MessageCreate,
+    once: false,
+    /**
+     * 
+     * @param {import('discord.js').Client} client 
+     * @param {import('discord.js').Message} message
+     */
+    async execute(client, message) {
+        if (message.author.id !== "438057969251254293") return;
+
+        const embeds = message.embeds;
+
+        if (embeds.length === 0) return;
+
+        const embed = embeds[0];
+
+        if (!embed.description.includes('★')) return;
+
+        const forward = EmbedBuilder.from(embed)
+
+        const guild = await client.guilds.fetch(process.env.GUILD_ID)
+        const chan = await guild.channels.fetch('1411333244598878312');
+
+        chan.send({ embeds: [forward], content: `[Go](${message.url})\n\n` });
+    }
+}
